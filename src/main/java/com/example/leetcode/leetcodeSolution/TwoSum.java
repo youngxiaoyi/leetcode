5ed0c346b55fcbc04ca1c0f60a53041bf0bd3e23
yangxaioyi <2971377570@qq.com>
2022-05-11 14:46:22 +0800

    [leetcode]两数之和

---
+++ src/main/java/com/example/leetcode/leetcodeSolution/TwoSum.java
@@ -0,0 +1,28 @@
+package com.example.leetcode.leetcodeSolution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ */
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[] {i, (int) map.get(nums[i])};
+            } else {
+                map.put(target- nums[i], i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+        int[] ints = twoSum(nums, target);
+        System.out.println("---");
+    }
+}
