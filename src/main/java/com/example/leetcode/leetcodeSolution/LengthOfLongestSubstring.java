e7a1535cf1f6c9eee37587cd0bf4a9d51d439c7a
yangxaioyi <2971377570@qq.com>
2022-05-11 16:21:10 +0800

    [leetcode]无重复字符的最长子串

---
+++ src/main/java/com/example/leetcode/leetcodeSolution/LengthOfLongestSubstring.java
@@ -0,0 +1,44 @@
+package com.example.leetcode.leetcodeSolution;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 无重复字符的最长子串
+ *
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
+public class LengthOfLongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        //队列
+        Deque<Character> deque = new ArrayDeque<>();
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            //没有字符往队列中添加元素
+            if (!deque.contains(s.charAt(i))) {
+                deque.addFirst(s.charAt(i));
+            }
+            //已经存在元素则从头删除到该元素，然后添加
+            else {
+                boolean isContinueDel = true;
+                while (isContinueDel) {
+                    char delChar = deque.removeLast();
+                    if (delChar == s.charAt(i)) {
+                        isContinueDel = false;
+                    }
+                }
+                deque.addFirst(s.charAt(i));
+            }
+            //统计当前存在是否为最长
+            max = Math.max(max, deque.size());
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int i = lengthOfLongestSubstring("pwwkew");
+        System.out.println("---");
+
+    }
+}
