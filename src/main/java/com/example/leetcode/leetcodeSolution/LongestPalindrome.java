7102235db4258d457aa16759c4ae26e4d75ec5b6
yangxaioyi <2971377570@qq.com>
2022-05-13 19:36:48 +0800

    [leetcode]最长回文子串-动态规划法

--- src/main/java/com/example/leetcode/leetcodeSolution/LongestPalindrome.java
+++ src/main/java/com/example/leetcode/leetcodeSolution/LongestPalindrome.java
@@ -45,10 +45,43 @@ public class LongestPalindrome {
         return result;
     }
 
+    /**
+     * 动态规划
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome2(String s) {
+        int len = s.length();
+//        dp[i][j]表示第i个元素到第j个元素是否是回文
+        int[][] dp = new int[len][len];
+        int max = 1;
+        int maxi = 0;
+        int maxj = 0;
 
+        //初始化，一个元素一定是回文
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = 1;
+        }
+        for (int j = 0; j < len; j++){
+            for(int i = 0; i < j; i++){
+                if (s.charAt(i) == s.charAt(j) && ((j-i<=2) || (dp[i+1][j-1] == 1))) {
+                    dp[i][j] = 1;
+
+                    //最长计算
+                    if (j-i+1 > max) {
+                        maxi = i;
+                        maxj = j;
+                        max = j-i+1;
+                    }
+                }
+            }
+        }
+        return s.substring(maxi, maxj+1);
+    }
 
     public static void main(String[] args) {
-        String babad = longestPalindrome("cccccc");
+        String babad = longestPalindrome("babad");
+        String babad2 = longestPalindrome2("babad");
         System.out.println("---");
     }
 }
